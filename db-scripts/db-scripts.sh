73087c5ccd8f1b1702b5e8863a6cc802a6d0bd8d
DharmendraRathor <31398624+DharmendraRathor@users.noreply.github.com>
2019-10-07 20:29:43 -0700

    updated script for table creation

--- db-scripts/db-scripts.sh
+++ db-scripts/db-scripts.sh
@@ -2,9 +2,29 @@
 CREATE TABLE dataengproj.locations_meta_data
 (
     location_id integer NOT NULL,
-	detail varchar,
+    detail varchar,
     geometry GEOMETRY(Point,4326),
     CONSTRAINT locations_meta_data_pkey1 PRIMARY KEY (location_id)
+)
+
+CREATE TABLE dataengproj.taxi_travel_score
+(
+ 	location_id integer,
+	event_time timestamp,
+	distance_avg integer,
+	psg_count_avg integer,
+	amount_avg integer,
+	count integer,
+    score integer,
+	CONSTRAINT unq_location_eventime_key1 UNIQUE(location_id,event_time)
+)
+
+CREATE TABLE dataengproj.accident_Score
+(
+    location_id integer,
+    event_time timestamp without time zone,
+    accident_type integer,
+    accident_description character varying(200) COLLATE pg_catalog."default"
 )
 
 CREATE TABLE dataengproj.crime_arrest_score
@@ -16,6 +36,8 @@ CREATE TABLE dataengproj.crime_arrest_score
     score integer,
 	CONSTRAINT unq_location_eventime_crime_ky_cd UNIQUE(location_id,event_time,crime_ky_cd)
 )
+
+
 
 
 #delete table
